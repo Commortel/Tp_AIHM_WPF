8719196f633a463c2e603cd64c67bda2c168ef73
Meyer Thibaut <contact@thibaut-meyer.fr>
2013-05-07 02:36:26 +0200

    Implement ElevatorButton

--- TP_AIHM/TP_AIHM/Elevator/ElevatorButton.xaml.cs
+++ TP_AIHM/TP_AIHM/Elevator/ElevatorButton.xaml.cs
@@ -11,50 +11,74 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Controls.Primitives;
 
 namespace TP_AIHM
 {
     /// <summary>
     /// Interaction logic for ElevatorButton.xaml
     /// </summary>
-    public partial class ElevatorButton : UserControl
+    public partial class ElevatorButton : ToggleButton
     {
-        public String name { get; set; }
-        public String iconPath 
-        {
-            set
-            {
-                this.ButtonImage.Source = this.CreateImage(value);
-            }
-        }
-        public String iconPathSelected 
-        {
-            set
-            {
-                this.ButtonImage.Source = this.CreateImage(value);
-            }
-        }
-        public ElevetorControl control { get; set; }
 
         public ElevatorButton()
         {
             InitializeComponent();
         }
 
-        private BitmapImage CreateImage(string path)
+        internal static readonly DependencyProperty EnabledUncheckedProperty = DependencyProperty.Register("EnabledUnchecked", typeof(BitmapImage), typeof(ElevatorButton));
+
+        internal static readonly DependencyProperty DisabledUncheckedProperty = DependencyProperty.Register("DisabledUnchecked", typeof(BitmapImage), typeof(ElevatorButton));
+
+        internal static readonly DependencyProperty EnabledCheckedProperty = DependencyProperty.Register("EnabledChecked", typeof(BitmapImage), typeof(ElevatorButton));
+
+        public BitmapImage EnabledUnchecked
         {
-            BitmapImage image = new BitmapImage();
-            image.BeginInit();
-            image.CacheOption = BitmapCacheOption.OnLoad;
-            image.CreateOptions = BitmapCreateOptions.IgnoreImageCache;
-            image.UriSource = new Uri(@"" + path, UriKind.Relative);
-            image.EndInit();
-            return image;
+            get { return (BitmapImage)GetValue(EnabledUncheckedProperty); }
+            set { SetValue(EnabledUncheckedProperty, value); }
         }
 
-        private void Button_Click(object sender, RoutedEventArgs e)
+        public BitmapImage DisabledUnchecked
         {
-            //this.control.
+            get { return (BitmapImage)GetValue(DisabledUncheckedProperty); }
+            set { SetValue(DisabledUncheckedProperty, value); }
+        }
+
+        public BitmapImage EnabledChecked
+        {
+            get { return (BitmapImage)GetValue(EnabledCheckedProperty); }
+            set { SetValue(EnabledCheckedProperty, value); }
+        }
+
+        private void ToggleButton_CheckedChanged(object sender, RoutedEventArgs e)
+        {
+            ChangeImage();
+        }
+
+        private void ToggleButton_IsEnabledChanged(object sender, DependencyPropertyChangedEventArgs e)
+        {
+            ChangeImage();
+        }
+
+        private void ToggleButton_Loaded(object sender, RoutedEventArgs e)
+        {
+            ChangeImage();
+        }
+
+        private void ChangeImage()
+        {
+            Console.WriteLine("Change");
+            if (IsEnabled)
+            {
+                if (IsChecked == true)
+                    ButtonImage.Source = EnabledChecked;
+                else
+                    ButtonImage.Source = EnabledUnchecked;
+            }
+            else
+            {
+                ButtonImage.Source = DisabledUnchecked;
+            }
         }
     }
 }
